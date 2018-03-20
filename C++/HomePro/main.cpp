99e9ad56c6b94bea6a0449a9f91dc77037b18bd4
Viliars <redkillerpw@mail.ru>
2018-03-20 10:52:34 +0300

    Наследование

---
+++ C++/HomePro/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include<string>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+
+class nix
+{
+int len_name;
+public:
+    int year;
+    char* name;
+    virtual void info()=0;
+    nix(const char* name,int year ){ this->len_name=strlen(name); this->name=new char[len_name]; strcpy(this->name,name); this->year=year; cout<<"Kernel created"<<endl; }
+    //nix(const nix& another) {  if(another.len_name!=this->len_name) {  delete[] this->name; this->name=new char[another.len_name]; } strcpy(this->name,another.name);  }
+    virtual ~nix() { delete[] name;
+    cout<<"DELETE KERNEL"<<endl; }
+};
+class Unix:public virtual nix
+{
+    int len_version;
+public:
+    char* version;
+    void info() { cout<<"I am "<<name<<" "<<version<<endl; }
+    Unix(const char* version,int year,const char* name="Unix"): nix(name,year)
+    {
+        len_version=strlen(version); this->version=new char[len_version]; strcpy(this->version,version); cout<<"Unix created"<<endl;
+    }
+    Unix(const nix& another)
+    {
+        if(another.len_name!=this->len_name) {  delete[] this->name; this->name=new char[another.len_name]; } strcpy(this->name,another.name);
+        if(another.len_version!=this->len_version) {  delete[] this->version; this->version=new char[another.len_version]; } strcpy(this->version,another.version);
+    }
+    ~Unix() { delete[] version;
+    cout<<"DELETE_UNIX "<<name<<" "; }
+};
+
+class BSD:public virtual nix
+{
+public:
+    int version;
+    int year;
+    void info() { cout<<"I am "<<version<<name<<endl; }
+    BSD(int version,int year,const char* name="BSD"): nix(name,year) { this->version=version; cout<<"BSD created"<<endl; }
+    ~BSD() { cout<<"DELETE_BSD "<<name<<" ";  }
+};
+
+class NewBSD:public BSD
+{
+public:
+    char* ver;
+    void info() { cout<<"I am "<<ver<<name<<" "<<version<<endl;  }
+    NewBSD(const char* ver,int version,int year,const char* name="BSD"): nix(name,year),BSD(version,year,name)
+    {
+        this->ver=new char[strlen(ver)]; strcpy(this->ver,ver);
+    }
+    ~NewBSD() { delete[] ver;
+    cout<<"DELETE_NEW ";}
+};
+
+class NewUnix:public Unix,public BSD
+{
+public:
+    void info() { cout<<"I am "<<BSD::version<<name<<" "<<Unix::version<<endl; }
+    static int count;
+    NewUnix(const char* version,int year,const char* name="Unix",int ver=1): nix(name,year),Unix(version,year,name), BSD(ver,year,name) {
+        count++;
+    }
+    ~NewUnix() {   cout<<"DELETE_NEW2 "; }
+};
+
+int NewUnix::count=0;
+int main()
+{
+    Unix V0("PDP7",1969,"Unics");
+    Unix V1("Version 1",1971,"Unix");
+    Unix V2("Version 2",1972);
+    Unix V3("Version 3",1973);
+    Unix V4("Version 4",1973);
+    Unix V5("Version 5",1974);
+    Unix V6("PWB",1974);
+    Unix V7("Version 6",1975);
+    BSD B0(1,1978);
+    BSD B1(2,1979);
+    NewBSD B3("Net",1,1993);
+    NewBSD B4("Net",3,2005);
+    NewBSD B5("Open",2,1996);
+    NewBSD B6("Open",3,2006);
+    NewBSD B7("Free",1,1993);
+    NewBSD B8("Free",2,1995);
+    NewBSD B9("Free",3,1998);
+    NewBSD B10("Free",6,2006);
+    Unix L0("0.0.1",1991,"Linux");
+    Unix L1("GNU",1983,"Linux");
+    NewUnix NU0("Version 8",1985,"Unix",8);
+    return 0;
+}
